b8a7a14da2d18df8e078520a9d92df848ced48cc
rakasadiantara <sadiantara86@gmail.com>
2021-06-15 04:26:07 +0700

    coba

--- frontend/src/component/LoginRegis/Regis.js
+++ frontend/src/component/LoginRegis/Regis.js
@@ -1,47 +1,47 @@
-// import React from 'react';
-// import '../../css/Regis.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import {faInstagram, faFacebook} from '@fortawesome/fontawesome-free-brands';
+import React from 'react';
+import '../../css/Regis.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faInstagram, faFacebook} from '@fortawesome/fontawesome-free-brands';
 
-// function Regis() {
-// return (
-// 	<div class="container" id="container">
-// 		<div class="form-container regis log-in-container">
-// 				<h1>Register</h1>
-//                 <br/>
-//                 <span>Please Register Your Identity</span>
-//                 <hr/>
-//                 <form method="post" action="">
-// 				<div class="social-container">
-// 				<h1>Register</h1>
+function Regis() {
+return (
+	<div class="container" id="container">
+		<div class="form-container regis log-in-container">
+				<h1>Register</h1>
+                <br/>
+                <span>Please Register Your Identity</span>
+                <hr/>
+                <form method="post" action="">
+				<div class="social-container">
+				<h1>Register</h1>
                 
-//                 <span>Please Register Your Identity</span>
-//                 <hr/>
-// 					<a href="#" class="social"><FontAwesomeIcon className ='font-awesome' icon={faFacebook} /></a>
-// 					<a href="#" class="social"><FontAwesomeIcon className ='font-awesome' icon={faInstagram} /></a>
-// 				</div>
+                <span>Please Register Your Identity</span>
+                <hr/>
+					<a href="#" class="social"><FontAwesomeIcon className ='font-awesome' icon={faFacebook} /></a>
+					<a href="#" class="social"><FontAwesomeIcon className ='font-awesome' icon={faInstagram} /></a>
+				</div>
 
-//                 <label for="username" class="col-sm-2 col-form-label"></label>
-//                 <input type="text" class="form-control" id="username" name="username" placeholder="Username" />
-//                         <label for="nama" class="col-sm-2 col-form-label"></label>
-//                 <input type="text" class="form-control" id="nama" name="nama" placeholder="Name" />
-//                 <label for="password" class="col-sm-2 col-form-label"></label>
-//                 <input type="password" class="form-control" id="password" name="pasword" placeholder="Password" />
-//                         <a href="#">Forgot your password?</a>
-// 						<button type="submit" class="btn btn-primary regis" name="register">Register</button>
-//                         <a href="/login" class="btn btn-success regis">LOGIN</a>
+                <label for="username" class="col-sm-2 col-form-label"></label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username" />
+                        <label for="nama" class="col-sm-2 col-form-label"></label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="Name" />
+                <label for="password" class="col-sm-2 col-form-label"></label>
+                <input type="password" class="form-control" id="password" name="pasword" placeholder="Password" />
+                        <a href="#">Forgot your password?</a>
+						<button type="submit" class="btn btn-primary regis" name="register">Register</button>
+                        <a href="/login" class="btn btn-success regis">LOGIN</a>
                         
-// 			</form>
-// 		</div>
-// 		<div class="overlay-container">
-// 			<div class="overlay">
-// 				<div class="overlay-panel overlay-right">
-// 				<div className="boxT"><h1>Thrift-In</h1></div>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	</div>
-// );
-// }
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-right">
+				<div className="boxT"><h1>Thrift-In</h1></div>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+}
 
-// export default Regis;
+export default Regis;
