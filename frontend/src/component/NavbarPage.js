e033eb819a4d4c45e794c012ede98f2d168808b1
jihanghaniyyah <72118791+jihanghaniyyah@users.noreply.github.com>
2021-06-15 16:59:30 +0700

    hai

--- frontend/src/component/NavbarPage.js
+++ frontend/src/component/NavbarPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Form, Navbar, Nav, FormControl, NavDropdown, InputGroup, Button, Image } from 'react-bootstrap';
-import gambar from '../asset/logo.png';
+import gambar from '../asset/logo2.png';
 import gambartas from '../asset/keranjang.png';
-import mega from '../asset/user.svg';
+import logouser from '../asset/logouser.png';
 import '../css/Navbar.css';
 
 function NavbarPage() {
@@ -12,7 +12,7 @@ function NavbarPage() {
                 {/* Ngethrift */}
                 <img
                     src={gambar}
-                    width="110"
+                    width="200"
                     className="d-inline-block align-top mx-3"
                     alt="DOSAR"
                 />
@@ -21,20 +21,19 @@ function NavbarPage() {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Form inline className=" mx-auto">
                     <FormControl type="text" placeholder="Search" className="searchtxt searchbar"/>
-                    <a href="/"><Button className="searchbtn searchbar">Search</Button></a>
+                    <a href="/"><Button className="searchbtn searchbar">Go</Button></a>
                 </Form>
-                
                      <Nav className="mx-auto">
                                     <Nav.Link href="/cart" className="d-flex align-items-center mr-5">
                                         <img
                                             src={gambartas}
-                                            width="30px"
-                                            height="35px"
+                                            width="35px"
+                                            height="30px"
                                             className="d-inline-block align-top"
                                             alt="DOSAR"
                                         />
                                         <a className="ml-2" style={{ position: "relative" }}>
-                                            <p className="m-0"><b>Kantong Belanja</b></p>
+                                            {/* <p className="m-0"><b>Kantong Belanja</b></p> */}
                                             <div className="d-flex justify-content-center align-items-center" style={{ 
                                                 position: "absolute", 
                                                 left: "-20px",
@@ -48,10 +47,12 @@ function NavbarPage() {
                                             </div> 
                                         </a>
                                     </Nav.Link>
+                                    <div className="username p-3">
+                                        Jihan
+                                    </div>
                                     <NavDropdown title=
-                                            { <Image width="35" src={mega} roundedCircle /> }
-                                            id="basic-nav-dropdown">
-                                      
+                                            { <Image width="40" height="40" src={logouser} roundedCircle /> }
+                                            id="basic-nav-dropdown">    
                                         <NavDropdown.Item href="/transactions">Riwayat Transaksi</NavDropdown.Item>
                                         <NavDropdown.Item href="/login" >Logout</NavDropdown.Item>
                                         {/* <NavDropdown.Divider />
