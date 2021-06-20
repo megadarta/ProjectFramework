b5dd2250af8546aec17bff48e3c463474ecc21ed
jihanghaniyyah <72118791+jihanghaniyyah@users.noreply.github.com>
2021-06-20 22:45:23 +0700

    halo

--- frontend/src/component/NavbarPage.js
+++ frontend/src/component/NavbarPage.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import { Form, Navbar, Nav, FormControl, NavDropdown, InputGroup, Button, Image } from 'react-bootstrap';
-import gambar from '../asset/logo.png';
+import gambar from '../asset/logo2.png';
 import gambartas from '../asset/keranjang.png';
-import mega from '../asset/user.svg';
+import logouser from '../asset/logouser.png';
 import '../css/Navbar.css';
 
-import { useHistory } from 'react-router';
-
-function NavbarPage(kirim) {
-    
-    const history = useHistory();
-
-    function logout(){
-        sessionStorage.removeItem("user");
-    }
-    function klikkeranjang(){
-        history.push(`/chekout?iduser=${kirim.user.id_user}`);
-    }
+function NavbarPage() {
     return (
         <Navbar className="p-3 dosar-nav" expand="lg" sticky="top">
             <Navbar.Brand href="/" className="">
                 {/* Ngethrift */}
                 <img
                     src={gambar}
-                    width="110"
+                    width="200"
                     className="d-inline-block align-top mx-3"
                     alt="DOSAR"
                 />
@@ -32,20 +21,19 @@ function NavbarPage(kirim) {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Form inline className=" mx-auto">
                     <FormControl type="text" placeholder="Search" className="searchtxt searchbar"/>
-                    <a href="/"><Button className="searchbtn searchbar">Search</Button></a>
+                    <a href="/"><Button className="searchbtn searchbar">Go</Button></a>
                 </Form>
-                
                      <Nav className="mx-auto">
-                                    <Nav.Link onClick={klikkeranjang} className="d-flex align-items-center mr-5">
+                                    <Nav.Link href="/cart" className="d-flex align-items-center mr-5">
                                         <img
                                             src={gambartas}
-                                            width="30px"
-                                            height="35px"
+                                            width="35px"
+                                            height="30px"
                                             className="d-inline-block align-top"
                                             alt="DOSAR"
                                         />
                                         <a className="ml-2" style={{ position: "relative" }}>
-                                            <p className="m-0"><b>Kantong Belanja</b></p>
+                                            {/* <p className="m-0"><b>Kantong Belanja</b></p> */}
                                             <div className="d-flex justify-content-center align-items-center" style={{ 
                                                 position: "absolute", 
                                                 left: "-20px",
@@ -59,13 +47,14 @@ function NavbarPage(kirim) {
                                             </div> 
                                         </a>
                                     </Nav.Link>
-                                    <div className="p-3">{kirim.user?.nama_user}</div>
+                                    <div className="username p-3">
+                                        Jihan
+                                    </div>
                                     <NavDropdown title=
-                                            { <Image width="35" src={mega} roundedCircle /> }
-                                            id="basic-nav-dropdown">
-                                      
+                                            { <Image width="40" height="40" src={logouser} roundedCircle /> }
+                                            id="basic-nav-dropdown">    
                                         <NavDropdown.Item href="/transactions">Riwayat Transaksi</NavDropdown.Item>
-                                        <NavDropdown.Item href="/login" onClick={logout}>Logout</NavDropdown.Item>
+                                        <NavDropdown.Item href="/login" >Logout</NavDropdown.Item>
                                         {/* <NavDropdown.Divider />
                                         <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item> */}
                                     </NavDropdown>
